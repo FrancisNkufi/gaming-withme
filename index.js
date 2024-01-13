cb03764176c256584dbc5d205b0f2196b6cbb214
Francis Nkufi <154917198+FrancisNkufi@users.noreply.github.com>
2024-01-13 23:57:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ var randImage2 = "images/" + ranIma2;
 
 document.querySelectorAll("img")[1].setAttribute("src",randImage2);
 
-if(randImage > randImage2){document.querySelector("h1").innerHTML = "Player 1 wins! ";}
-else if(randImage2 > randImage){document.querySelector("h1").innerHTML = "Player 2 wins!";}
-else{document.querySelector("h1").innerHTML = "It a Draw";}
+if(randImage > randImage2){document.querySelector("h1").innerHTML = "Player 1 wins!🚩🚩";}
+else if(randImage2 > randImage){document.querySelector("h1").innerHTML = "Player 2 wins!"🏳‍🌈🏳‍🌈;}
+else{document.querySelector("h1").innerHTML = "It a Draw 🚦";}
